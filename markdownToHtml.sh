032f81a0823d5cd673a9374f51541227035a618c
Kevin A. Mitchell <kam@datalogics.com>
2014-08-21 10:00:53 -0500

    Set LANG explicitly to include UTF-8 for markdown processing
    - It can be set to just en_US from inside of Vim

--- markdownToHtml.sh
+++ markdownToHtml.sh
@@ -2,5 +2,6 @@
 
 export GEM_HOME=$HOME/.gem
 export PATH=$HOME/.gem/bin:$HOME/Library/Python/2.7/bin:$PATH
+export LANG=en_US.UTF-8
 here=`dirname "$0"`
-rdiscount $1 | /usr/bin/python "$here"/highlight_code.py | tee $TMPDIR/test.html
+rdiscount $1 | /usr/bin/python "$here"/highlight_code.py
